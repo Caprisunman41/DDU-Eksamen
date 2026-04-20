dae77975dc14130958d96380dc29d9aebd096c8f
sigurdhp06 <sigurdhp06@gmail.com>
2026-04-20 12:25:20 +0200

    Mini fix
    Quit game virker i editor nu

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -11,8 +11,11 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame()
     {
-        Debug.Log("Quit Game");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
-    
+
 }

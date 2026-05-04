fb5e976c07f1029579736b2bc154022e768b43c3
Zach <zach020506@gmail.com>
2026-05-04 12:40:42 +0200

    background music

---
+++ Assets/Scripts/Background_music.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Background_music : MonoBehaviour
+{
+    public static Background_music instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
